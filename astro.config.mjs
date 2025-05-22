a54f53f2f687ea043609e92a15714984d8c8bbbc
S.R <abel20100311@gmail.com>
2025-05-22 17:59:38 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+	site : process.env.URL  ?? 'https://SebasZwolf.github.io',
+	base : process.env.BASE ?? '/demo-neomedica',
 });
